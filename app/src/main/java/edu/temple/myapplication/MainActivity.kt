ffc83e95f5b469953e6c1c3fdcd4928f7c59e1e6
tui62689 <112034701+tui62689@users.noreply.github.com>
2024-11-14 14:54:26 -0500

    adjustments to bind

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -13,31 +13,40 @@ class MainActivity : AppCompatActivity() {
     lateinit var timerBinder: TimerService.TimerBinder
     private var isConnected = false
 
+    val serviceConnection = object : ServiceConnection{
+        override fun onServiceConnected(p0: ComponentName?, service: IBinder?) {
+            timerBinder = service as TimerService.TimerBinder
+            isConnected = true
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            isConnected = false
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val serviceConnection = object : ServiceConnection{
-            override fun onServiceConnected(p0: ComponentName?, service: IBinder?) {
-                timerBinder = service as TimerService.TimerBinder
-                isConnected = true
-            }
 
-            override fun onServiceDisconnected(p0: ComponentName?) {
-                isConnected = false
-            }
-        }
+
+        bindService(
+            Intent(this, TimerService::class.java), serviceConnection
+            , BIND_AUTO_CREATE
+        )
 
         findViewById<Button>(R.id.startButton).setOnClickListener {
-            bindService(
-                Intent(this, TimerService::class.java), serviceConnection
-                , BIND_AUTO_CREATE
-            )
+
 
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
 
         }
+    }
+
+    override fun onDestroy() {
+        unbindService(serviceConnection)
+        super.onDestroy()
     }
 }
